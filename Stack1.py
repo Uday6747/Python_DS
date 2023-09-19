8c2cab7bb9843f46c4990aa219387ba35d5f360a
UDAY <udaykumarchinni2706@gmail.com>
2023-09-19 19:16:43 +0530

    Simple program of implementation of Stack using Lists

---
+++ Stack1.py
@@ -0,0 +1,13 @@
+# Implementation of Stack using Lists
+
+stack = []
+stack.append(10)
+stack.append(20)
+stack.append(30)
+stack.append(40)
+
+print(f"Stack : {stack}")
+print(f"Length of the Stack : {len(stack)}")
+
+print(f"Poped Element : {stack.pop()}")
+print(f"After the Pop operation : {stack}")
