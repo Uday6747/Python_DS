e6fa69ec9b1e80d73bd1e140056132575a2a09f1
UDAY <udaykumarchinni2706@gmail.com>
2023-09-28 20:28:32 +0530

    Queue implementation using Lists

---
+++ QueueLists.py
@@ -0,0 +1,17 @@
+# Queue using Lists
+
+queue = []
+print(f"Length of the Queue : {len(queue)}")
+
+queue.append(10)
+queue.append(20)
+queue.append(30)
+queue.append(40)
+
+print(f"Queue : {queue}")
+
+print(f"Poped Element : {queue.pop(0)}")
+print(f"Queue after the pop operation : {queue}")
+print(f"Poped Element : {queue.pop(0)}")
+print(f"Queue after the pop operation : {queue}")
+
